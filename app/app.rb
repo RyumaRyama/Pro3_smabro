6e0ad3630f04ca791ea0559365bb59d3ce0fc66f
RyumaRyama <e165730@ie.u-ryukyu.ac.jp>
2019-05-16 14:34:02 +0900

    add mysql2 to Gemfile

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,9 @@
 require "sinatra"
 require "sinatra/reloader"
+require "mysql2"
 
 get "/" do
+  hoge
   "Hello, from Docker. My Ruby version is: #{RUBY_VERSION}"
 end
 
@@ -11,4 +13,15 @@ end
 
 get "/akari" do
   '\ｱｯｶﾘ~ﾝ/ '*1000
+end
+
+def hoge
+  client = Mysql2::Client.new(
+    host: "db",
+    username: "root",
+    password: "password",
+    port: "3306",
+    database: "database"
+  )
+  p client.query("SHOW TABLES")
 end
