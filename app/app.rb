771291a44e4b0e6b24bc42ce5aea4ddf7c4761d3
RyumaRyama <e165730@ie.u-ryukyu.ac.jp>
2019-05-18 23:47:45 +0900

    connect to the database onluy once

--- app/app.rb
+++ app/app.rb
@@ -1,37 +1,62 @@
-require "sinatra"
-require "sinatra/reloader"
-require "mysql2"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'mysql2'
+require 'socket'
+
+# dbのポートにアクセスできたらtrueを返す
+def connecting_db
+  begin
+  TCPSocket.open('db', 3306)
+  rescue
+    false
+  else
+    true
+  end
+end
+
+# dbに繋げられる状態まで1秒ずつ待つ
+while !connecting_db
+  sleep 1
+end
+
+client = Mysql2::Client.new(
+  host: 'db',
+  username: 'root',
+  password: 'password',
+  port: '3306',
+  database: 'db'
+)
 
 # 一度だけ実行されるらしい
 configure do
 end
 
-get "/" do
-  fuga = ""
-  client = connect_db_client
-  client.query("SHOW TABLES").to_s
-  client.query("SELECT * FROM users").each do |hoge|
-    fuga += "<p>" + hoge["name"] + "</p>"
+get '/' do
+  fuga = ''
+  # client = connect_db_client
+  client.query('SHOW TABLES').to_s
+  client.query('SELECT * FROM users').each do |hoge|
+    fuga += '<p>' + hoge['name'] + '</p>'
   end
   fuga
-  # "Hello, from Docker. My Ruby version is: #{RUBY_VERSION}"
+  # 'Hello, from Docker. My Ruby version is: #{RUBY_VERSION}'
 end
 
-get "/hello" do
-  "This is a new contents."
+get '/hello' do
+  'This is a new contents.'
 end
 
-get "/akari" do
+get '/akari' do
   '\ｱｯｶﾘ~ﾝ/ '*1000
 end
 
-def connect_db_client
-  Mysql2::Client.new(
-    host: "db",
-    username: "root",
-    password: "password",
-    port: "3306",
-    database: "db"
-  )
-end
+# def connect_db_client
+#   Mysql2::Client.new(
+#     host: 'db',
+#     username: 'root',
+#     password: 'password',
+#     port: '3306',
+#     database: 'db'
+#   )
+# end
 
