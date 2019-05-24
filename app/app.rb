3b26cc9a231b392667d7205304efd7d44d2e07e8
RyumaRyama <e165730@ie.u-ryukyu.ac.jp>
2019-05-24 15:49:59 +0900

    delete mysql container

--- app/app.rb
+++ app/app.rb
@@ -4,47 +4,46 @@ require 'mysql2'
 require 'socket'
 
 # dbのポートにアクセスできたらtrueを返す
-def connecting_db
-  begin
-  TCPSocket.open('db', 3306)
-  rescue
-    false
-  else
-    true
-  end
-end
-
-# dbに繋げられる状態まで1秒ずつ待つ
-while !connecting_db
-  sleep 1
-end
-
-client = Mysql2::Client.new(
-  host: 'db',
-  username: 'root',
-  password: 'password',
-  port: '3306',
-  database: 'db'
-)
+# def connecting_db
+#   begin
+#   TCPSocket.open('db', 3306)
+#   rescue
+#     false
+#   else
+#     true
+#   end
+# end
+#
+# # dbに繋げられる状態まで1秒ずつ待つ
+# while !connecting_db
+#   sleep 1
+# end
+#
+# client = Mysql2::Client.new(
+#   host: 'db',
+#   username: 'root',
+#   password: 'password',
+#   port: '3306',
+#   database: 'db'
+# )
 
 # 一度だけ実行されるらしい
 configure do
 end
 
 get '/' do
-  fuga = ''
+  '\ｱｯｶﾘ~ﾝ/ '*100
   # client = connect_db_client
-  client.query('SHOW TABLES').to_s
-  client.query('SELECT * FROM users').each do |hoge|
-    fuga += '<p>' + hoge['name'] + '</p>'
-  end
-  fuga
+  # client.query('SHOW TABLES').to_s
+  # client.query('SELECT * FROM users').each do |hoge|
+  #   fuga += '<p>' + hoge['name'] + '</p>'
+  # end
+  # fuga
   # 'Hello, from Docker. My Ruby version is: #{RUBY_VERSION}'
 end
 
 get '/hello' do
-  'This is a new contents.'
-end
+  'This is a new contents.' end
 
 
 get "/gorakubu/akari" do
