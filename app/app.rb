249b73f0aecb62a7e5776f9ef3d2438a4d75f3b3
RyumaRyama <e165730@ie.u-ryukyu.ac.jp>
2019-05-25 19:25:29 +0900

    change database

--- app/app.rb
+++ app/app.rb
@@ -1,45 +1,57 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'mysql2'
+require 'pg'
 require 'socket'
 
+p ENV["PORT"]
+set :port, ENV["PORT"]
+
 # dbのポートにアクセスできたらtrueを返す
-# def connecting_db
-#   begin
-#   TCPSocket.open('db', 3306)
-#   rescue
-#     false
-#   else
-#     true
-#   end
-# end
-#
-# # dbに繋げられる状態まで1秒ずつ待つ
-# while !connecting_db
-#   sleep 1
-# end
-#
-# client = Mysql2::Client.new(
-#   host: 'db',
-#   username: 'root',
-#   password: 'password',
-#   port: '3306',
-#   database: 'db'
-# )
+def connecting_db
+  begin
+  TCPSocket.open(ENV['POSTGRES_HOST'], 5432)
+  rescue
+    false
+  else
+    true
+  end
+end
+
+# dbに繋げられる状態まで1秒ずつ待つ
+while !connecting_db
+  sleep 1
+end
+
+p ENV['POSTGRES_HOST']
+p ENV['POSTGRES_USER']
+p ENV['POSTGRES_PASSWORD']
+p ENV['POSTGRES_NAME']
+
+client = PG::connect(
+  host: ENV['POSTGRES_HOST'],
+  user: ENV['POSTGRES_USER'],
+  password: ENV['POSTGRES_PASSWORD'],
+  port: '5432',
+  dbname: ENV['POSTGRES_NAME']
+)
 
 # 一度だけ実行されるらしい
 configure do
+  client.exec("CREATE TABLE IF NOT EXISTS users (name TEXT);")
+  # client.exec("INSERT INTO users (name) SELECT '\ｱｯｶﾘ~ﾝ/';")
+  # client.exec("INSERT INTO users (name) SELECT 'Kyoko';")
+  # client.exec("INSERT INTO users (name) SELECT 'Yui';")
+  # client.exec("INSERT INTO users (name) SELECT 'Chinatsu';")
 end
 
 get '/' do
-  '\ｱｯｶﾘ~ﾝ/ '*100
-  # client = connect_db_client
-  # client.query('SHOW TABLES').to_s
-  # client.query('SELECT * FROM users').each do |hoge|
-  #   fuga += '<p>' + hoge['name'] + '</p>'
-  # end
-  # fuga
-  # 'Hello, from Docker. My Ruby version is: #{RUBY_VERSION}'
+  # '\ｱｯｶﾘ~ﾝ/ '*100
+  client.exec("INSERT INTO users (name) SELECT '\\ｱｯｶﾘ~ﾝ/';")
+  fuga = ''
+  client.query('SELECT * FROM users').each do |hoge|
+    fuga += hoge['name'] + " "
+  end
+  fuga
 end
 
 get '/hello' do
