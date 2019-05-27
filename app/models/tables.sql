93d2549f32e01d773a6832b8f018518003699567
RyumaRyama <e165730@ie.u-ryukyu.ac.jp>
2019-05-27 20:08:30 +0900

    reset fighters table every time boot

--- app/models/tables.sql
+++ app/models/tables.sql
@@ -1,13 +1,15 @@
 -- fighterモデル
-CREATE TABLE IF NOT EXISTS fighters (
-    id SERIAL NOT NULL, -- 自動連番
+-- 初期化
+DROP TABLE IF EXISTS fighters;
+CREATE TABLE fighters (
+    id INTEGER NOT NULL UNIQUE, -- 自動連番
     name TEXT NOT NULL UNIQUE, -- 重複禁止
     PRIMARY KEY (id)
 );
 
 -- usersモデル
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL UNIQUE,
     name TEXT NOT NULL,
     PRIMARY KEY (id)
 );
